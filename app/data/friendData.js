906128fc00e3400566d4943968d0b7ddb3d13758
Megan Evans <megan.evans.ducky@gmail.com>
2018-08-16 16:10:10 -0600

    added images

--- app/data/friendData.js
+++ app/data/friendData.js
@@ -1,35 +1,82 @@
-
 var friendData = [{
-    name: "Scrooge McDuck",
-    photo: "#",
-    survey: [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-    ]
-},{
-    name: "Donald Duck",
-    photo: "#",
-    survey: [
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-    ]
-}
+        name: "Scrooge McDuck",
+        photo: "./images/scrooge.png",
+        survey: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }, {
+        name: "Donald Duck",
+        photo: "./images/donald.png",
+        survey: [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+        ]
+    },
+    {
+        name: "Daffy Duck",
+        photo: "./images/daffy.png",
+        survey: [
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3
+        ]
+    },
+    {
+        name: "Daisy Duck",
+        photo: "./images/daisy.png",
+        survey: [
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+        ]
+    },
+    {
+        name: "Heuy Duck",
+        photo: "./images/Huey.png",
+        survey: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+        ]
+    }
 ]
 
 module.exports = friendData;
