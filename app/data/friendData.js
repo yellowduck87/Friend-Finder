a1351dececa69ad44b2b901086763f82efc1fc09
Megan Evans <megan.evans.ducky@gmail.com>
2018-08-18 17:16:26 -0600

    added style changes

--- app/data/friendData.js
+++ app/data/friendData.js
@@ -1,6 +1,6 @@
 var friendData = [{
         name: "Scrooge McDuck",
-        photo: "https://vignette.wikia.nocookie.net/scroogemcduck/images/3/31/DT2017_Scrooge_McDuck.png/revision/latest?cb=20171203141439",
+        photo: "/images/scrooge.png",
         survey: [
             1,
             1,
@@ -15,7 +15,7 @@ var friendData = [{
         ]
     }, {
         name: "Donald Duck",
-        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/220px-Donald_Duck.svg.png",
+        photo: "/images/donald.png",
         survey: [
             2,
             2,
@@ -31,7 +31,7 @@ var friendData = [{
     },
     {
         name: "Daffy Duck",
-        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Daffy_Duck.svg/1200px-Daffy_Duck.svg.png",
+        photo: "/images/daffy.png",
         survey: [
             3, 
             3, 
@@ -47,7 +47,7 @@ var friendData = [{
     },
     {
         name: "Daisy Duck",
-        photo: "https://vignette.wikia.nocookie.net/disney/images/6/67/Daisy_Duck_transparent.png/revision/latest?cb=20160508175748",
+        photo: "/images/daisy.png",
         survey: [
            4,
            4,
@@ -63,7 +63,7 @@ var friendData = [{
     },
     {
         name: "Heuy Duck",
-        photo: "https://vignette.wikia.nocookie.net/scroogemcduck/images/4/40/Huey_Ducktales.png/revision/latest?cb=20171008073659",
+        photo: "/images/Huey.png",
         survey: [
             5,
             5,
